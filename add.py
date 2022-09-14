362858e148fb40336f4b8564912903821e38fc91
Jian Gao <gaoj20@mails.tsinghua.edu.cn>
2022-09-14 20:11:54 +0800

    initial commit

---
+++ add.py
@@ -0,0 +1,9 @@
+'''
+Implements a custom integer add function.
+'''
+
+def int_add(lhs, rhs):
+    '''
+    Adds lhs and rhs together and returns the sum.
+    '''
+    return lhs + rhs
---
+++ test.py
@@ -0,0 +1,4 @@
+from add import int_add
+def test_add():
+    assert int_add(2, 3) == 5
+    assert int_add(-1, 1) == 0
